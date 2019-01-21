668cde0883a624a4612932697f84f11b4c646fc3
yuka <u1_qsh1tq_way@yahoo.co.jp>
2019-01-21 09:33:55 +0000

    DRY

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190115131748) do
+ActiveRecord::Schema.define(version: 20190121092004) do
 
   create_table "tasks", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "Status"
   end
 
 end
